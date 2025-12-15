ee95261e7c1cbbe51bcdb52f9574ca814ca4aa06
Tech-devops18 <irfancareers18@gmail.com>
2025-12-15 20:59:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Starting Go application")
+	fmt.Println("Start Go application")
 
 	SayHello()
 	result := Add(10, 20)
 
 	fmt.Println("Addition result:", result)
 }
+
 
